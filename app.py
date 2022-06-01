8fe98159fbe6566e02c60c6d47363e016696bb93
3Drapa <3Drapa@gmail.com>
2022-06-01 15:29:11 +0200

    adedd fory ify

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/jinja-fory")
+def jinja_fory():
+    lista=["11111", "222222", "33333", "44444"]
+    return render_template("jinja-fory.html", jlista=lista)
+
+@app.route("/jinja-ify")
+def jinja_ify():
+    nr=22
+    return render_template("jinja-ify.html", jnr=nr)
+
 if __name__ == "__main__":
     app.run(debug=True)
